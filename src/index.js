f0b844650272e8d7158d5004339fbfd0dfd9b513
sahlminkok <sahlminkok7@gmail.com>
2023-06-07 14:28:42 +0100

    Make footer to stay at the bottom of the page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import './styles/main.scss';
 import logo from './assets/movie-logo.png';
-import footerLogo from './assets/logo-open.jpg';
 
 const headerLogo = document.querySelector('#logo');
 const footerImg = document.querySelector('.footer-logo');
 
 headerLogo.src = logo;
-footerImg.src = footerLogo;
+footerImg.src = logo;
