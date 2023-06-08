1e9cd5cc5fe4b69a042f96df4482f3ab0862a026
sahlminkok <sahlminkok7@gmail.com>
2023-06-08 08:24:51 +0100

    Add the like button

--- src/modules/displayShows.js
+++ src/modules/displayShows.js
@@ -1,18 +1,27 @@
+import likeImg from '../assets/heart.svg';
+
 const displayShows = (shows) => {
   const showsList = document.querySelector('.shows-list');
   showsList.innerHTML = '';
-  shows.forEach((result) => {
+  for (let i = 0; i < 8; i += 1) {
+    const show = shows[i];
     const div = document.createElement('div');
     div.className = 'show-item';
     div.innerHTML = `
-      <img src="${result.image.original}" alt="${result.name}" class="show-image">
-      <p>${result.name}</p>
+      <img src="${show.image.original}" alt="${show.name}" class="show-image">
+      <div class="info">
+        <p>${show.name}</p>
+        <div class="likes">
+          <img alt="likes-btn" src="${likeImg}" class="likes-btn">
+          <p>10 likes</p>
+        </div>
+      </div>
       <button>Comments</button>
       <button>Reservations</button>
     `;
 
     showsList.appendChild(div);
-  });
+  }
 };
 
 export default displayShows;
