036758ea0e9746010d4f8d016ade4f0fe0215386
Simpleshaikh1 <Hamzatbolaji648@yahoo.com>
2023-06-10 15:35:31 +0100

    test for reservation working fine

--- src/test/reserve.test.js
+++ src/test/reserve.test.js
@@ -1,14 +1,33 @@
-import reserveCounter from '../modules/reserveCounter.js';
+// Import the necessary modules and functions
+import reservationCounter from '../modules/reserveCounter.js';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-const { JSDOM } = require('jsdom');
-
-const dom = new JSDOM('<!DOCTYPE html><p id="reservation-counter">()</p>');
-global.document = dom.window.document;
-
-test('Should display the number of reservations made', () => {
-  const mockList = [1, 2];
-  const counter = document.getElementById('reservation-counter');
-  reserveCounter(mockList);
-  expect(counter.innerHTML).toBe('(2)');
+describe('reservationCounter', () => {
+  // Mock the document and its properties
+  beforeEach(() => {
+    document.body.innerHTML = '<div id="reservation-counter"></div>';
+  });
+  // Clean up the document after each test
+  afterEach(() => {
+    document.body.innerHTML = '';
+  });
+  it('should update the reservation counter with the correct length', () => {
+    // Create a sample list of reservations
+    const reservations = ['reservation1', 'reservation2', 'reservation3'];
+    // Call the reservationCounter function
+    reservationCounter(reservations);
+    // Get the updated counter element
+    const counter = document.getElementById('reservation-counter');
+    // Assert that the counter has been updated with the correct length
+    expect(counter.innerHTML).toBe('(3)');
+  });
+  it('should update the reservation counter with zero when the list is empty', () => {
+    // Create an empty list of reservations
+    const reservations = [];
+    // Call the reservationCounter function
+    reservationCounter(reservations);
+    // Get the updated counter element
+    const counter = document.getElementById('reservation-counter');
+    // Assert that the counter has been updated with zero
+    expect(counter.innerHTML).toBe('(0)');
+  });
 });
